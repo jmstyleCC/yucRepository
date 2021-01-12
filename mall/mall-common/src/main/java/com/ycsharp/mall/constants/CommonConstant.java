340d8e77f5169af1cd308898ddda203dde13095a
sam <18314385714@139.com>
2021-01-12 11:13:20 +0800

    测试

---
+++ mall/mall-common/src/main/java/com/ycsharp/mall/constants/CommonConstant.java
@@ -0,0 +1,11 @@
+package com.ycsharp.mall.constants;
+
+/**
+ * @ClassName: CommonConstant
+ * @Author: YU CONG
+ * @Description:
+ * @Date: 2021/1/12 10:52
+ * @Company:浪潮软件集团
+ **/
+public class CommonConstant {
+}
